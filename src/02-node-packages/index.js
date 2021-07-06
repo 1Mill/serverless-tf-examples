2fafe4453fab58e7be980a9ce221aa629395d01b
1Mill <erik.ekberg@outlook.com>
2021-07-05 18:20:29 -0700

    Update index.js

--- src/02-node-packages/index.js
+++ src/02-node-packages/index.js
@@ -2,6 +2,6 @@ const { fromTheDepths } = require('./utils/from-the-depths')
 const { v4: uuidv4 } = require('uuid')
 
 exports.handler = (_event, _ctx) => {
-	console.log(`Hello world! - ${uuidv4()}`)
+	console.log(`Hello World! - ${uuidv4()}`)
 	fromTheDepths()
 }
