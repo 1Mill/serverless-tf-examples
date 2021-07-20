67557390d5a82be7ac94f48a173b05ee968d3849
1Mill <erik.ekberg@outlook.com>
2021-07-20 15:09:00 -0700

    Rename build docker-coimpose file

--- scripts/lambda/build.0.bash
+++ scripts/lambda/build.0.bash
@@ -6,7 +6,7 @@ function build() {
 	local CONFIG_FILE_PATH=$(pwd)/$1/$CONFIG_FILENAME
 	source $CONFIG_FILE_PATH
 
-	local FILE=$(dirname $BASH_SOURCE)/docker-compose.yml
+	local FILE=$(dirname $BASH_SOURCE)/build.docker-compose.0.yml
 	local FUNCTION_NAME=$FUNCTION_NAME
 
 	DIR=$(pwd)/$1 \
