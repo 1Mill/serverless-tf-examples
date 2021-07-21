3a67c217af8427aa18ce47ab87925e7630e3643e
1Mill <erik.ekberg@outlook.com>
2021-07-21 12:18:52 -0700

    Change lambda/run implemention

--- scripts/lambda/run.0.bash
+++ scripts/lambda/run.0.bash
@@ -2,8 +2,9 @@ function run() {
 	local DIR=$1
 	local VERSION=$2
 	local ACTION=$3
+	local PAYLOAD="$4"
 
-	source $(dirname $BASH_SOURCE)/$ACTION.$VERSION.bash $DIR
+	source $(dirname $BASH_SOURCE)/$ACTION.$VERSION.bash $DIR "$PAYLOAD"
 }
 
-run $1 $2 $3
+run $1 $2 $3 "$4"
